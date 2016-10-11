1d7340ab230c4add8f02cc48dd36f014e3d0852a
Seth Lakowske <lakowske@gmail.com>
2016-10-10 23:55:15 -0500

    vbox-script.sh

--- vbox-script.sh
+++ vbox-script.sh
@@ -8,13 +8,13 @@ VBoxManage storagectl $VM_NAME --name "SATA Controller" --add sata --controller 
 VBoxManage storageattach $VM_NAME --storagectl "SATA Controller" --port 0   --device 0 --type hdd --medium $VM_VDI
 
 # No need to add installer iso for tin machine copies
-#VBoxManage storagectl $VM --name "IDE Controller" --add ide
-#VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0   --device 0 --type dvddrive --medium $VM_ISO
-VBoxManage modifyvm $VM --ioapic on
-VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
-VBoxManage modifyvm $VM --memory 2048 --vram 128
-VBoxManage modifyvm $VM --nic1 nat
-VBoxManage modifyvm $VM --nic2 hostonly --hostonlyadapter2 vboxnet1
-VBoxManage showvminfo $VM
-VBoxManage modifyvm $VM --vrde on
-#VBoxHeadless -s $VM
+#VBoxManage storagectl $VM_NAME --name "IDE Controller" --add ide
+#VBoxManage storageattach $VM_NAME --storagectl "IDE Controller" --port 0   --device 0 --type dvddrive --medium $VM_ISO
+VBoxManage modifyvm $VM_NAME --ioapic on
+VBoxManage modifyvm $VM_NAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+VBoxManage modifyvm $VM_NAME --memory 2048 --vram 128
+VBoxManage modifyvm $VM_NAME --nic1 nat
+VBoxManage modifyvm $VM_NAME --nic2 hostonly --hostonlyadapter2 vboxnet1
+VBoxManage showvminfo $VM_NAME
+VBoxManage modifyvm $VM_NAME --vrde on
+#VBoxHeadless -s $VM_NAME
